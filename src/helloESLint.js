29306309edb5b57c4b4612e41947bd4144927e1a
yoshiken0927 <51016793+yoshiken0927@users.noreply.github.com>
2021-02-15 00:02:50 +0900

    Update helloESLint.js

--- src/helloESLint.js
+++ src/helloESLint.js
@@ -1,5 +1,11 @@
 function sum(a, b) {
-  return a + c;
+  return a + b;
+//  return a + c;
 }
 
-console.log(sum(1, 2));
+console.log(
+  sum(
+    1, sum(2, sum(3, sum(4, sum(5, sum(6, sum(7, sum(8, sum(9, sum(10, 11))))))))),
+  ),
+);
+// console.log(sum(1, sum(2,sum(3,sum(4,sum(5,sum(6,sum(7,sum(8,sum(9,sum(10,11)))))))))));
